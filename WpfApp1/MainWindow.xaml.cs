0386c4ef6f5457435242c8223dfcda4a1dc82023
Mehran Abdi <mehranabdi1379@gmail.com>
2023-03-13 13:04:45 +0330

    Practicing serilog basics

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeriLogPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            const string customTemplate = "Will be logged: {Timestamp:yyyy-MMM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}";
+
+            ILogger logger = new LoggerConfiguration()
+                .Destructure.ByTransforming<Color>(c => new {c.Red , c.Green})
+                .WriteTo
+                .File("log.txt" , outputTemplate: customTemplate)
+                .CreateLogger();
+
+            Log.Logger = logger;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = Name.Text;
+            int age = int.Parse(Age.Text);
+            var faveColor = new Color
+            {
+                Blue = 132,
+                Green = 13,
+                Red = 24
+            };
+
+            Log.Information("User Added {UserName} , Age {UserAge}. Favorites: {@Colors}" , name, age , faveColor);
+
+        }
+    }
+
+    public class Color
+    {
+        public int Green { get; set; }
+        public int Blue { get; set; }
+        public int Red { get; set; }
+    }
+}
